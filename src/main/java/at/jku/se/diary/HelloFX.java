db79c04e4509f7284d68bb1932c78c7fddabff7b
serdaryalcin7 <serdar2000@live.at>
2022-04-24 18:08:58 +0200

    diary

--- src/main/java/at/jku/se/diary/HelloFX.java
+++ src/main/java/at/jku/se/diary/HelloFX.java
@@ -15,6 +15,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.control.TextArea;
 
 import javafx.scene.image.Image;
 
@@ -26,7 +27,8 @@ public class HelloFX extends Application {
     Button button, button2;
     Scene scene1, scene2;
     TableView<Entries> tableView;
-    TextField title, location, date, text;
+    TextField title, location, date;
+    TextArea text;
 
     public static void main(String[] args) {
         launch();
@@ -45,6 +47,56 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         window = stage;
 
+        TableColumn<Entries, String> titleColumn = new TableColumn<>("Title");
+        titleColumn.setMinWidth(200);
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+
+        TableColumn<Entries, String> locationColumn = new TableColumn<>("Location");
+        locationColumn.setMinWidth(200);
+        locationColumn.setCellValueFactory(new PropertyValueFactory<>("location"));
+
+        TableColumn<Entries, String> dateColumn = new TableColumn<>("Date");
+        dateColumn.setMinWidth(200);
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        TableColumn<Entries, String> textColumn = new TableColumn<>("Text");
+        textColumn.setMinWidth(200);
+        textColumn.setCellValueFactory(new PropertyValueFactory<>("text"));
+
+        title = new TextField();
+        title.setPromptText("title");
+        title.setMinWidth(100);
+
+        location = new TextField();
+        location.setPromptText("location");
+        location.setMinWidth(100);
+
+        date = new TextField();
+        date.setPromptText("date");
+        date.setMinWidth(100);
+
+        text = new TextArea();
+        text.setPromptText("text");
+        text.setMinWidth(200);
+
+
+
+        Button addButton = new Button("Add new Entry");
+        addButton.setOnAction(e-> addButtonClicked());
+        Button deleteButton = new Button("Delete entry");
+        deleteButton.setOnAction(e-> deleteButtonClicked());
+
+        HBox hBox = new HBox();
+        hBox.setPadding(new Insets(10,10,10,10));
+        hBox.setSpacing(10);
+        hBox.getChildren().addAll(title, location, date, text, addButton, deleteButton);
+
+        tableView = new TableView<>();
+        tableView.getColumns().addAll(titleColumn, locationColumn, dateColumn, textColumn);
+
+        VBox vbox = new VBox();
+        vbox.getChildren().addAll(hBox, tableView);
+
 
         StackPane layout = new StackPane();
 
@@ -58,6 +110,11 @@ public class HelloFX extends Application {
         Background bGround = new Background(bImg);
         layout.setBackground(bGround);
 
+        scene2 = new Scene(vbox, 900, 600);
+        button2 = new Button("go back");
+        button2.setOnAction(e-> window.setScene(scene1));
+        window.setScene(scene2);
+        window.show();
 
 
         button = new Button();
@@ -72,6 +129,24 @@ public class HelloFX extends Application {
         stage.show();
     }
 
+    public void addButtonClicked(){
+        Entries entries = new Entries();
+        entries.setTitle(title.getText());
+        entries.setLocation(location.getText());
+        entries.setDate(date.getText());
+        entries.setText(text.getText());
+        tableView.getItems().add(entries);
+        title.clear();
+        location.clear();
+        date.clear();
+        text.clear();
+    }
 
+    public void deleteButtonClicked(){
+        ObservableList<Entries> entrySelected, allEntries;
+        allEntries = tableView.getItems();
+        entrySelected = tableView.getSelectionModel().getSelectedItems();
+        entrySelected.forEach(allEntries::remove);
+    }
 
 }
