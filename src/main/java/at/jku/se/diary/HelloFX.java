93b2347ea9b7eb4159832d2b749a69c7a6c8f241
Abi646 <rifat_kilicaslan@live.at>
2022-04-25 21:31:25 +0200

    ComboBox for the "Tag" Option implemented,but still not complete

--- src/main/java/at/jku/se/diary/HelloFX.java
+++ src/main/java/at/jku/se/diary/HelloFX.java
@@ -11,6 +11,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
@@ -19,16 +22,23 @@ import javafx.scene.control.TextArea;
 
 import javafx.scene.image.Image;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.net.PortUnreachableException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Locale;
 
 public class HelloFX extends Application {
     Stage window;
     Button button,button1, button2,button3,button4;
     Scene scene,scene1, scene2;
     TableView<Entries> tableView;
-    TextField title, location, date;
+    TextField title, location,date;
     TextArea text;
+    ComboBox tag;
 
     public static void main(String[] args) {
         launch();
@@ -58,11 +68,11 @@ public class HelloFX extends Application {
         button1 = new Button();
         button1.setText("New entry");
         button2 = new Button();
-        button2.setText("Search Entry");
+        button2.setText("Search entry");
         button3 = new Button();
         button3.setText("Search for a category");
         button4 = new Button();
-        button4.setText("show map");
+        button4.setText("Show map");
 
         button.setFont(Font.font("Arial", 25));
         button1.setFont(Font.font("Arial", 25));
@@ -108,6 +118,11 @@ public class HelloFX extends Application {
         textColumn.setMinWidth(200);
         textColumn.setCellValueFactory(new PropertyValueFactory<>("text"));
 
+        TableColumn<Entries, String> tagColumn = new TableColumn<>("Tag");
+        tagColumn.setMinWidth(200);
+        tagColumn.setCellValueFactory(new PropertyValueFactory<>("tag"));
+
+
         title = new TextField();
         title.setPromptText("title");
         title.setMinWidth(100);
@@ -116,42 +131,54 @@ public class HelloFX extends Application {
         location.setPromptText("location");
         location.setMinWidth(100);
 
-        date  = new TextField();
-        date .setPromptText("date");
-        date .setMinWidth(100);
+
+        date = new TextField();
+        date.setPromptText("DD/MM/YYYY");
+        date.setMinWidth(100);
 
         text = new TextArea();
         text.setPromptText("text");
         text.setMinWidth(200);
 
+
+        tag = new ComboBox<>();
+        final String [] comboBoxItems = {"Hotel", "Restaurant", "Beach"};
+        tag.setEditable(true);
+        tag.setValue("Add a tag");
+        tag.getItems().addAll(comboBoxItems);
+
+
+
         Button addButton = new Button("Add new entry");
         addButton.setOnAction(e-> addButtonClicked());
-        Button back = new Button("back");
-        back.setOnAction(e-> window.setScene(scene));
+        Button back1 = new Button("Go back");
+        back1.setOnAction(e-> window.setScene(scene));
+        Button back2 = new Button("Go back");
+        back2.setOnAction(e-> window.setScene(scene));
         Button deleteButton = new Button("Delete entry");
         deleteButton.setOnAction(e-> deleteButtonClicked());
-        Button addentry = new Button("Add new Entry");
-        addentry.setOnAction(e-> window.setScene(scene1));
+        Button addEntry = new Button("Add new Entry");
+        addEntry.setOnAction(e-> window.setScene(scene1));
 
 
         HBox hBox = new HBox();
         hBox.setPadding(new Insets(10,10,10,10));
         hBox.setSpacing(10);
-        hBox.getChildren().addAll(title, location, date, text,addButton, back);
+        hBox.getChildren().addAll(title, location, date, text, addButton, back1, tag);
 
         tableView = new TableView<>();
-        tableView.getColumns().addAll(titleColumn, locationColumn, dateColumn,textColumn);
+        tableView.getColumns().addAll(titleColumn, locationColumn, dateColumn,textColumn, tagColumn);
 
 
         VBox vbox = new VBox();
         vbox.getChildren().addAll(hBox);
 
-        scene1 = new Scene(vbox,900,600);
+        scene1 = new Scene(vbox,1200,750);
         window.setScene(scene);
 
         VBox vbox2 = new VBox();
-        vbox2.getChildren().addAll(tableView,addentry ,deleteButton);
-        scene2 = new Scene(vbox2, 900, 600);
+        vbox2.getChildren().addAll(tableView, addEntry ,deleteButton, back2);
+        scene2 = new Scene(vbox2, 1200, 750);
 
 
     }
@@ -162,6 +189,7 @@ public class HelloFX extends Application {
         entries.setLocation(location.getText());
         entries.setDate(date.getText());
         entries.setText(text.getText());
+        entries.setTag(String.valueOf(tag.getValue()));
         tableView.getItems().add(entries);
         title.clear();
         location.clear();
