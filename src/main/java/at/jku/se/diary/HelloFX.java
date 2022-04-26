a220777e3fbc81f3a568f1ed8f7019fa746ed3b6
Abi646 <rifat_kilicaslan@live.at>
2022-04-26 21:25:39 +0200

    some updates

--- src/main/java/at/jku/se/diary/HelloFX.java
+++ src/main/java/at/jku/se/diary/HelloFX.java
@@ -1,44 +1,35 @@
-/*
- * Source Code must not be used without permission of
- * Reinhold Plösch (reinhold.ploesch@gmail.com)
- * (c) Reinhold Plösch, 2020
- */
 package at.jku.se.diary;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.scene.control.TextArea;
-
 import javafx.scene.image.Image;
+import java.util.function.Predicate;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.net.PortUnreachableException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.Locale;
+
 
 public class HelloFX extends Application {
     Stage window;
     Button button,button1, button2,button3,button4;
-    Scene scene,scene1, scene2;
-    TableView<Entries> tableView;
-    TextField title, location,date;
+    Scene scene,scene1, scene2,scene3;
+    TableView<Entries> tableView,tableView1;
+    TextField title, location, searchField;
+    DatePicker date;
     TextArea text;
+
     ComboBox tag;
+    ObservableList<Entries> data = FXCollections.observableArrayList();
 
     public static void main(String[] args) {
         launch();
@@ -47,13 +38,12 @@ public class HelloFX extends Application {
     @Override
     public void start(Stage stage) {
         //----------------Scene: HOMEPAGE--------------------
-
         window = stage;
 
-        stage.setTitle("DIARY_FX");
-        HBox layout = new HBox();
+        window.setTitle("DIARY_FX");
+        HBox hBox = new HBox();
 
-        scene = new Scene(layout, 1200,750);
+        scene = new Scene(hBox, 1200, 750);
         Image img = new Image("https://i.f1g.fr/media/cms/1200x630_crop/2022/02/21/806d983eb123652ee94b2027b8f5487924b2133636999a3dcb56b5236db51093.png");
         BackgroundImage bImg = new BackgroundImage(img,
                 BackgroundRepeat.NO_REPEAT,
@@ -61,18 +51,18 @@ public class HelloFX extends Application {
                 BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT);
         Background bGround = new Background(bImg);
-        layout.setBackground(bGround);
+        hBox.setBackground(bGround);
 
-        button= new Button();
+        button = new Button();
         button.setText("Show overview");
         button1 = new Button();
         button1.setText("New entry");
         button2 = new Button();
-        button2.setText("Search entry");
+        button2.setText("Search Entry");
         button3 = new Button();
         button3.setText("Search for a category");
         button4 = new Button();
-        button4.setText("Show map");
+        button4.setText("show map");
 
         button.setFont(Font.font("Arial", 25));
         button1.setFont(Font.font("Arial", 25));
@@ -80,27 +70,23 @@ public class HelloFX extends Application {
         button3.setFont(Font.font("Arial", 25));
         button4.setFont(Font.font("Arial", 25));
 
-        button1.setOnAction(e-> window.setScene(scene1));
-        button.setOnAction(e-> window.setScene(scene2));
+        button.setOnAction(e -> window.setScene(scene2));
+        button1.setOnAction(e -> window.setScene(scene1));
+        button2.setOnAction(e -> window.setScene(scene3));
 
-        layout.getChildren().addAll(button,button1, button2,button3,button4);
-        layout.setPadding(new Insets(5,5,5,5));
-        layout.setSpacing(10);
-        layout.setAlignment(Pos.CENTER);
-        stage.setScene(scene);
-        window.show();
-        stage.setTitle("DIARY");
-        stage.show();
+        hBox.getChildren().addAll(button, button1, button2, button3, button4);
+        hBox.setPadding(new Insets(5, 5, 5, 5));
+        hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER);
 
 
-        //----------------Scence 1: New entry--------------------
+        /*----------------Scence 1: New entry--------------------*/
+        Label label1 = new Label("Titel: ");
+        Label label2 = new Label("Date: ");
+        Label label3 = new Label("Location: ");
+        Label label4 = new Label("Text: ");
 
-
-
-
-
-        //----------------Scence 2: Overview--------------------
-
+        /*----------------Scence 2: Overview--------------------*/
 
         TableColumn<Entries, String> titleColumn = new TableColumn<>("Title");
         titleColumn.setMinWidth(200);
@@ -124,22 +110,20 @@ public class HelloFX extends Application {
 
 
         title = new TextField();
-        title.setPromptText("title");
+        title.setPromptText("title...");
         title.setMinWidth(100);
 
         location = new TextField();
-        location.setPromptText("location");
+        location.setPromptText("location...");
         location.setMinWidth(100);
 
-
-        date = new TextField();
-        date.setPromptText("DD/MM/YYYY");
+        date = new DatePicker();
+        date.setPromptText("date...");
         date.setMinWidth(100);
 
         text = new TextArea();
-        text.setPromptText("text");
+        text.setPromptText("text...");
         text.setMinWidth(200);
-
 
         tag = new ComboBox<>();
         final String [] comboBoxItems = {"Hotel", "Restaurant", "Beach"};
@@ -147,53 +131,105 @@ public class HelloFX extends Application {
         tag.setValue("Add a tag");
         tag.getItems().addAll(comboBoxItems);
 
-
-
         Button addButton = new Button("Add new entry");
-        addButton.setOnAction(e-> addButtonClicked());
-        Button back1 = new Button("Go back");
-        back1.setOnAction(e-> window.setScene(scene));
-        Button back2 = new Button("Go back");
-        back2.setOnAction(e-> window.setScene(scene));
+        addButton.setOnAction(e -> {
+            addButtonClicked();
+            System.out.println("Entry have been created!");
+        });
+        Button back1 = new Button("back");
+        back1.setOnAction(e -> window.setScene(scene));
         Button deleteButton = new Button("Delete entry");
-        deleteButton.setOnAction(e-> deleteButtonClicked());
-        Button addEntry = new Button("Add new Entry");
-        addEntry.setOnAction(e-> window.setScene(scene1));
+        deleteButton.setOnAction(e -> deleteButtonClicked());
+        Button addentry = new Button("Add new Entry");
+        addentry.setOnAction(e -> window.setScene(scene1));
 
 
-        HBox hBox = new HBox();
-        hBox.setPadding(new Insets(10,10,10,10));
-        hBox.setSpacing(10);
-        hBox.getChildren().addAll(title, location, date, text, addButton, back1, tag);
+        VBox vBox = new VBox();
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        vBox.setSpacing(10);
+        vBox.getChildren().addAll(label1, title, label2, date, label3, location, label4, text, addButton, tag, back1);
 
         tableView = new TableView<>();
-        tableView.getColumns().addAll(titleColumn, locationColumn, dateColumn,textColumn, tagColumn);
+        tableView.getColumns().addAll(titleColumn, locationColumn, dateColumn, textColumn,tagColumn);
 
+        VBox vBox2 = new VBox();
+        vBox2.getChildren().addAll(vBox);
 
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(hBox);
+        scene1 = new Scene(vBox2, 1200, 750);
 
-        scene1 = new Scene(vbox,1200,750);
+        Button back2 = new Button("back");
+        back2.setOnAction(e -> window.setScene(scene));
+        VBox vBox3 = new VBox();
+        vBox3.setPadding(new Insets(10, 10, 10, 10));
+        vBox3.setSpacing(10);
+        vBox3.getChildren().addAll(tableView, addentry, deleteButton, back2);
+        scene2 = new Scene(vBox3, 1200, 750);
+
+        /*----------------Scence 3: Search--------------------*/
+        Label label5 = new Label("Search or Filter Table: ");
+        searchField = new TextField();
+        searchField.setPromptText("Search or Filter Table By title, date, location and Text...");
+        searchField.setMinWidth(100);
+
+        tableView1 = new TableView<>();
+        tableView1.getColumns().addAll(titleColumn, locationColumn, dateColumn, textColumn,tagColumn);
+
+        tableView1.setItems(data);
+
+        FilteredList<Entries> filteredData = new FilteredList<>(data, e -> true);
+        searchField.setOnKeyPressed(e -> {
+            searchField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+                filteredData.setPredicate((Predicate<? super Entries>) entries -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                        return true;
+                    }
+                    String lowerCaseFilter = newValue.toLowerCase();
+                    if (entries.getTitle().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else if (entries.getDate().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else if (entries.getLocation().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else if (entries.getText().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    } else if (entries.getTag().toLowerCase().contains(lowerCaseFilter)) {
+                        return true;
+                    }
+                    return false;
+                });
+            });
+
+            SortedList<Entries> sorteddata = new SortedList<>(filteredData);
+            sorteddata.comparatorProperty().bind(tableView1.comparatorProperty());
+            tableView1.setItems(sorteddata);
+
+        });
+
+        Button back3 = new Button("Go back");
+        back3.setOnAction(e -> window.setScene(scene));
+
+        VBox vBox4 = new VBox();
+        vBox4.setPadding(new Insets(10, 10, 10, 10));
+        vBox4.setSpacing(10);
+        vBox4.getChildren().addAll(label5, searchField, tableView1, back3);
+        scene3 = new Scene(vBox4, 1200, 750);
+
         window.setScene(scene);
-
-        VBox vbox2 = new VBox();
-        vbox2.getChildren().addAll(tableView, addEntry ,deleteButton, back2);
-        scene2 = new Scene(vbox2, 1200, 750);
-
-
+        window.show();
     }
 
-    public void addButtonClicked(){
+    public void addButtonClicked() {
         Entries entries = new Entries();
         entries.setTitle(title.getText());
+        entries.setDate(date.getEditor().getText());
         entries.setLocation(location.getText());
-        entries.setDate(date.getText());
         entries.setText(text.getText());
         entries.setTag(String.valueOf(tag.getValue()));
         tableView.getItems().add(entries);
+        tableView1.getItems().add(entries);
         title.clear();
+        date.setValue(null);
         location.clear();
-        date.clear();
         text.clear();
     }
 
